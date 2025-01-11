65ec993cb909bda5c35d4d667c689ac5bb37a5d0
raryjy <raryjy@gmail.com>
2025-01-11 16:08:27 +0100

    final exercise

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
     print("Deleting public directory...")
@@ -15,5 +19,7 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 main()
